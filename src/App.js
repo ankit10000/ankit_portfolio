fd4de68f308aa1dd3196f76df9cca639abb048ce
ankit10000 <ankitjangid9197@gmail.com>
2024-01-14 00:15:19 +0530

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/Home";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <Router>
       <Home />
+      <SpeedInsights />
     </Router>
   );
 }
