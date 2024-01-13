2e94e07cfe8c2f43ec875f9f54987ee79257e712
ankit10000 <ankitjangid9197@gmail.com>
2024-01-14 00:20:49 +0530

    added vercel analytics and before added speed insights

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/Home";
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <Router>
       <Home />
       <SpeedInsights />
+      <Analytics />
     </Router>
   );
 }
